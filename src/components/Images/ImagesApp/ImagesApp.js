1c76714b2643224b1a44276dbc68d9282fb9b963
davidnedkonst <david.ned.kon@gmail.com>
2023-12-05 20:27:23 +0100

    update Image App with useReducer

--- src/components/Images/ImagesApp/ImagesApp.js
+++ src/components/Images/ImagesApp/ImagesApp.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useReducer, useEffect } from "react";
 import Button from "../Button";
 import Loader from "../Loader";
 import ImageModal from "../ImageModal";
@@ -15,196 +15,220 @@ const initState = {
     total: 0,
     page: 1,
     perPage: 12,
+    activeImage: null,
+};
+
+const initStatus = {
+    status: Status.IDLE,
     showModal: false,
     showGallery: false,
     showButton: false,
     showLoader: false,
-    contentModal: null,
     error: null,
-    status: Status.IDLE,
+};
+
+const reducerState = (state, action) => {
+    switch (action.type) {
+        case 'query':
+            return ({ ...initState, query: action.query, });
+            break;
+
+        case 'image':
+            return ({ ...initState, image: [...state.image, ...action.image], });
+            break;
+
+        case 'page':
+            return ({ ...initState, page: state.page + 1, });
+            break;
+
+        case 'activeImage':
+            return ({ ...initState, activeImage: action.activeImage, });
+            break;
+
+        default:
+            break;
+    }
+};
+const reducerStatus = (state, action) => {
+    switch (action.type) {
+        case 'status':
+            return ;
+            break;
+
+        default:
+            break;
+    }
 };
 
 export default function ImagesApp({ time = 1000 }) {
 
     //General state
-    const [query, setQuery] = useState(initState.query);
-    const [image, setImage] = useState(initState.image);
-    const [total, setTotal] = useState(initState.total);
-    const [page, setPage] = useState(initState.page);
-    const [perPage, setPerPage] = useState(initState.perPage);
-    const [contentModal, setContentModal] = useState(initState.contentModal);
-    const [error, setError] = useState(initState.error);
+    const [state, setState] = useReducer(reducerState, initState);
+    const [status, setStatus] = useReducer(reducerStatus, initStatus);
 
-    //General status
-    const [status, setStatus] = useState(Status.IDLE);
-
-    //Showing state
-    const [showModal, setShowModal] = useState(initState.showModal);
-    const [showGallery, setShowGallery] = useState(initState.showGallery);
-    const [showButton, setShowButton] = useState(initState.showButton);
-    const [showLoader, setShowLoader] = useState(initState.showLoader);
-
-    //
-    const resetState = () => {
-        setQuery(initState.query);
-        setImage(initState.images);
-        setTotal(initState.total);
-        setPage(initState.page);
-        setPerPage(initState.perPage);
-        setShowModal(initState.showModal);
-        setContentModal(initState.contentModal);
-        setError(initState.error);
-        setStatus(Status.IDLE);
-    };
+    //Reset
+    // const resetState = () => {
+    //     setQuery(initState.query);
+    //     setImage(initState.images);
+    //     setTotal(initState.total);
+    //     setPage(initState.page);
+    //     setPerPage(initState.perPage);
+    //     setShowModal(initState.showModal);
+    //     setContentModal(initState.contentModal);
+    //     setError(initState.error);
+    //     setStatus(Status.IDLE);
+    // };
 
     const handleSubmit = query => {
-        resetState();
-        if (query) setQuery(query);
-    };
-
-    const handleLoadClick = event => {
-        if (image.length < total) {
-            setPage(s => s + 1);
+        if (query) {
+            setState({ type: 'query', query });
         }
     };
 
-    const handleImageClick = image => {
-        if (image) {
-            setShowModal(true);
-            setContentModal(image);
+    const handleLoadClick = event => {
+        const isShortageImage = state.image.length < state.total;
+        if (isShortageImage) {
+            setState({ type: 'page' });
+        }
+    };
+
+    const handleImageClick = activeImage => {
+        if (activeImage) {
+            setState({ type: 'activeImage', activeImage });
         }
     };
 
     const closeModal = () => {
-        setShowModal(initState.showModal);
-        setContentModal(initState.contentModal);
+        // setShowModal(initState.showModal);
+        setState({ type: 'activeImage', activeImage: initState.activeImage });
+        setStatus({ type: 'showModal', showModal: false, });
     };
 
     //Update status
-    useEffect(() => {
-        const isUpdateQuery = query !== initState.query;
-        if (isUpdateQuery) {
-            setError(initState.error);
-            setImage(initState.image);
-            setStatus(Status.PENDING);
-            console.log(Status.PENDING);
-        }
-    }, [query]);
+    // useEffect(() => {
+    //     const isUpdateQuery = query !== initState.query;
+    //     if (isUpdateQuery) {
+    //         setError(initState.error);
+    //         setImage(initState.image);
+    //         setStatus(Status.PENDING);
+    //         console.log(Status.PENDING);
+    //     }
+    // }, [query]);
 
-    useEffect(() => {
-        const runFetch = status === Status.PENDING || status === Status.LOADING;
-        // const i = image;
-        // console.log(i);
-        // const length = i.length;
-        // console.log("length = "+length);
-        // const isImage = length > 0;
-        if (runFetch && image.length > 0) {
-            setError(initState.error);
-            setStatus(Status.RESOLVED);
-            console.log(Status.RESOLVED);
-        }
-    }, [image, status]);
+    // useEffect(() => {
+    //     const runFetch = status === Status.PENDING || status === Status.LOADING;
+    //     // const i = image;
+    //     // console.log(i);
+    //     // const length = i.length;
+    //     // console.log("length = "+length);
+    //     // const isImage = length > 0;
+    //     if (runFetch && image.length > 0) {
+    //         setError(initState.error);
+    //         setStatus(Status.RESOLVED);
+    //         console.log(Status.RESOLVED);
+    //     }
+    // }, [image, status]);
 
-    useEffect(() => {
-        const isUpdatePage = page !== initState.page;
-        if (isUpdatePage) {
-            setStatus(Status.LOADING);
-            console.log(Status.LOADING);
-        }
-    }, [page]);
+    // useEffect(() => {
+    //     const isUpdatePage = page !== initState.page;
+    //     if (isUpdatePage) {
+    //         setStatus(Status.LOADING);
+    //         console.log(Status.LOADING);
+    //     }
+    // }, [page]);
 
-    useEffect(() => {
-        const isError = error ? true : false;
-        if (isError) {
-            setStatus(Status.REJECTED);
-            console.log(Status.REJECTED);
-        }
-    }, [error]);
+    // useEffect(() => {
+    //     const isError = error ? true : false;
+    //     if (isError) {
+    //         setStatus(Status.REJECTED);
+    //         console.log(Status.REJECTED);
+    //     }
+    // }, [error]);
 
     //Update showing
-    useEffect(
-        () => {
-            setShowGallery(status === Status.RESOLVED || status === Status.LOADING);
-        },
-        [image, total, status]
-    );
+    // useEffect(
+    //     () => {
+    //         setShowGallery(status === Status.RESOLVED || status === Status.LOADING);
+    //     },
+    //     [image, total, status]
+    // );
 
-    useEffect(
-        () => {
-            const length = image.length;
-            console.log("l = " + image.length);
-            const isShortageImage = length < total && total !== 0;
+    // useEffect(
+    //     () => {
+    //         const length = image.length;
+    //         console.log("l = " + image.length);
+    //         const isShortageImage = length < total && total !== 0;
 
-            setShowButton(status === Status.RESOLVED && (image.length < total && total > 0));
-        },
-        [image, total, status]
-    );
+    //         setShowButton(status === Status.RESOLVED && (image.length < total && total > 0));
+    //     },
+    //     [image, total, status]
+    // );
 
-    useEffect(
-        () => {
-            // const length = image.length;
-            // console.log("l = " + image.length);
-            // const isShortageImage = length < total && total !== 0;
+    // useEffect(
+    //     () => {
+    //         // const length = image.length;
+    //         // console.log("l = " + image.length);
+    //         // const isShortageImage = length < total && total !== 0;
 
-            setShowLoader(status === Status.PENDING || status === Status.LOADING);
-        },
-        [image, total, status]
-    );
+    //         setShowLoader(status === Status.PENDING || status === Status.LOADING);
+    //     },
+    //     [image, total, status]
+    // );
 
     //Reset error
-    useEffect(
-        () => {
-            const isNoError = status === Status.PENDING || status === Status.LOADING || status === Status.RESOLVED;
+    // useEffect(
+    //     () => {
+    //         const isNoError = status === Status.PENDING || status === Status.LOADING || status === Status.RESOLVED;
 
-            if (isNoError) setError(initState.error);
-        },
-        [error, status]
-    );
+    //         if (isNoError) setError(initState.error);
+    //     },
+    //     [error, status]
+    // );
 
     //Fetch
-    useEffect(
-        () => {
-            const runFetch = status === Status.PENDING || status === Status.LOADING;
+    // useEffect(
+    //     () => {
+    //         const runFetch = status.status === Status.PENDING || status.status === Status.LOADING;
 
-            if (runFetch) {
-                setTimeout(
-                    () => {
-                        fetchFromUrl(query, page, perPage)
-                            .then(
-                                ({ hits, totalHits }) => {
-                                    const shortageImage = totalHits - image.length;
-                                    const newImage = shortageImage >= perPage ? hits : hits.slice(0, shortageImage);
-                                    setImage([...image, ...newImage]);
-                                    setTotal(totalHits);
-                                }
-                            )
-                            .catch(error => setError(error));
-                    }, time
-                );
-            };
-        },
-        [query, page, perPage, image, error, status, time]
-    );
+    //         if (runFetch) {
+    //             setTimeout(
+    //                 () => {
+    //                     fetchFromUrl(state.query, state.page, state.perPage)
+    //                         .then(
+    //                             ({ hits, totalHits }) => {
+    //                                 const shortageImage = totalHits - image.length;
+    //                                 const newImage = shortageImage >= perPage ? hits : hits.slice(0, shortageImage);
+    //                                 setImage([...image, ...newImage]);
+    //                                 setTotal(totalHits);
+    //                             }
+    //                         )
+    //                         .catch(error => setError(error));
+    //                 }, time
+    //             );
+    //         };
+    //     },
+    //     [state.query, state.page, state.perPage, state.image, status.error, status.status, time]
+    // );
 
     return (
         <div className={css.ImagesApp}>
             <h2>ImagesApp</h2>
             <Searchbar onSubmit={handleSubmit} />
             <ImageGallery
-                show={showGallery}
-                image={image}
+                show={status.showGallery}
+                image={state.image}
                 onImageClick={handleImageClick}
             />
             <Button
-                show={showButton}
+                show={status.showButton}
                 onLoadClick={handleLoadClick}
             />
             <Loader
-                show={showLoader}
+                show={status.showLoader}
             />
             <ImageModal
-                show={showModal}
-                contentModal={contentModal}
+                show={status.showModal}
+                contentModal={state.contentModal}
                 onClose={closeModal}
             />
         </div>
